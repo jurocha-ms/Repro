0c5abf06fc1fe4dd679672348091cc7174001f5e
Julio C. Rocha <julio.rocha@microsoft.com>
2023-04-07 18:04:09 -0700

    Implement GuidHelper sample

---
+++ Windows/GuidHelper/GuidHelper/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <winrt/Windows.Foundation.h>
+
+using std::cout;
+using std::endl;
+using winrt::Windows::Foundation::GuidHelper;
+
+void main()
+{
+	try
+	{
+		auto guid = GuidHelper::CreateNewGuid();
+
+		auto hstr = winrt::to_hstring(guid);
+		auto str = winrt::to_string(hstr);
+
+		cout << "[WIN] " << str << endl;
+	}
+	catch (const winrt::hresult_error& e)
+	{
+		auto message = winrt::to_string(e.message());
+		cout << "[FAIL] " << e.code() << ": " << message << endl;
+	}
+}
