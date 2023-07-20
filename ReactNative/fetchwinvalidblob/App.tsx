c4b44d74b1491c7d71623f484471b039281d4411
Julio C. Rocha <julio.rocha@microsoft.com>
2023-07-19 23:53:41 -0700

    Finish trivial example

--- ReactNative/fetchwinvalidblob/App.tsx
+++ ReactNative/fetchwinvalidblob/App.tsx
@@ -2,19 +2,34 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * @see https://retool.com/blog/the-react-lifecycle-methods-and-hooks-explained
  * @format
  */
 
-//TODO: await (await fetch('https://raw.githubusercontent.com/microsoft/react-native-windows/main/.yarnrc.yml')).text()
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 
 const App = () => {
+
+	const [content, setContent] = useState('NOTHING')
+	const uri = 'https://raw.githubusercontent.com/microsoft/react-native-windows/main/.yarnrc.yml'
+
+	useEffect(() => {
+		const doFetch = async () => {
+			var response = await fetch(uri)
+			var text = await response.text()
+			setContent(text)
+		}
+
+		doFetch().catch(console.error)
+	}, [])
+
 	return (
 		<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-			<Text>Hello, world!</Text>
+			<Text>Content:</Text>
+			<Text>[{content}]</Text>
 		</View>
-	);
-};
+	)
+}
 
-export default App;
+export default App
