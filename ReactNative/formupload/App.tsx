5cd0cb8da3f058be715f6cb233fa6b127f886b52
Julio C. Rocha <julio.rocha@microsoft.com>
2023-09-22 18:37:56 -0700

    Implement as showcased in GH issue

--- ReactNative/formupload/App.tsx
+++ ReactNative/formupload/App.tsx
@@ -1,13 +1,14 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Multipart file upload test
  *
- * @see https://retool.com/blog/the-react-lifecycle-methods-and-hooks-explained
+ * @see https://github.com/microsoft/react-native-windows/issues/12168
  * @format
  */
 
 import React, {useEffect, useState} from 'react';
 import {Button, Text, View} from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
+import axios from 'axios';
 
 const App = () => {
 	const [content, setContent] = useState('NOTHING');
@@ -16,16 +17,54 @@ const App = () => {
 		'https://raw.githubusercontent.com/microsoft/react-native-windows/main/.yarnrc.yml';
 	//	'http://localhost:5555';
 
-	const doFetch = async () => {
-		var response = await fetch(uri);
-		var text = await response.text();
-		setReqId(reqId + 1);
-		setContent(text);
+	// const doFetch = async () => {
+	// 	var response = await fetch(uri);
+	// 	var text = await response.text();
+	// 	setReqId(reqId + 1);
+	// 	setContent(text);
+	// };
+
+	const uploadFile = async () => {
+		const pickerResult = await DocumentPicker.pickSingle({
+			presentationStyle: 'fullScreen',
+			copyTo: 'documentDirectory',
+			type: [DocumentPicker.types.allFiles],
+			mode: 'import',
+		});
+		console.log(pickerResult);
 	};
 
-	useEffect(() => {
-		doFetch().catch(console.error);
-	}, []);
+	const uploadImage = async (file) => {
+		const formData = new FormData();
+		formData.append('file', {
+			uri: file.fileCopyUri,
+			type: file.type,
+			name: file.name,
+			fileName: file.name,
+			size: file.size,
+		});
+		console.log(formData);
+
+		try {
+			const response = await axios.post(
+				'SOME URL',
+				formData,
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				},
+			);
+
+			console.log('Image upload success:', response.data);
+		} catch (error) {
+			console.error('Image upload error:', error);
+		}
+	};
+
+	// useEffect(() => {
+	// 	doFetch().catch(console.error);
+	// }, []);
 
 	return (
 		<View
@@ -34,9 +73,11 @@ const App = () => {
 				justifyContent: 'flex-start',
 				alignItems: 'stretch',
 			}}>
-			<Text>Response {reqId}:</Text>
+			{/* <Text>Response {reqId}:</Text>
 			<Text>[{content}]</Text>
-			<Button onPress={doFetch} title="Reload" />
+			<Button onPress={doFetch} title="Reload" /> */}
+			<Text>Chale</Text>
+			<Button onPress={uploadFile} title="Open" />
 		</View>
 	);
 };
