80fee59db45dd1ae32b170e41f2849eb2b4cdc79
Julio C. Rocha <julio.rocha@microsoft.com>
2024-04-24 15:57:06 -0700

    Add line breaks

--- Windows/DllArchDetector/HostApp/Main.cpp
+++ Windows/DllArchDetector/HostApp/Main.cpp
@@ -8,11 +8,11 @@ int main()
 // https://techcommunity.microsoft.com/t5/windows-os-platform-blog/getting-to-know-arm64ec-defines-and-intrinsic-functions/ba-p/2957235
 #ifdef _M_X64
 	auto messageIntel = MessageX64();
-	printf("[%s]", messageIntel);
+	printf("[%s]\n", messageIntel);
 #endif // _M_X64
 
 #ifdef _M_ARM64EC
 	auto messageArm = MessageARM64EC();
-	printf("[%s]", messageArm);
+	printf("[%s]\n", messageArm);
 #endif // _M_ARM64EC
 }
