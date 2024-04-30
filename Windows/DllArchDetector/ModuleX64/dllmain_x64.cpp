35966a8527f7a2679aac9b96f161178f59f362a5
Julio C. Rocha <julio.rocha@microsoft.com>
2024-04-30 15:24:35 -0700

    Add trivial statements to x64 DLL

--- Windows/DllArchDetector/ModuleX64/dllmain_x64.cpp
+++ Windows/DllArchDetector/ModuleX64/dllmain_x64.cpp
@@ -6,12 +6,22 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
                      )
 {
+    int x = 1;
+    int y = 2;
+    int z;
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        z = 0;
+        break;
     case DLL_THREAD_ATTACH:
+        z = 1;
+        break;
     case DLL_THREAD_DETACH:
+        z = 2;
+        break;
     case DLL_PROCESS_DETACH:
+        z = 3;
         break;
     }
     return TRUE;
