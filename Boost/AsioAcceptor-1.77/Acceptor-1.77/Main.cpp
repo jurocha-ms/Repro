556f85aa578c4a5b3a1d9830d06842dcdf79567a
Julio C. Rocha <julio.rocha@microsoft.com>
2024-07-31 20:38:46 -0700

    Define "Server" class"

--- Boost/AsioAcceptor-1.77/Acceptor-1.77/Main.cpp
+++ Boost/AsioAcceptor-1.77/Acceptor-1.77/Main.cpp
@@ -1,5 +1,38 @@
+//#include <thread>
+
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
+
+class Server : public std::enable_shared_from_this<Server>
+{
+    size_t m_ioThreadCount;
+    //std::vector<std::thread> m_ioThreads;
+    //boost::asio::io_context m_ioContext;
+    //boost::asio::ip::tcp::acceptor m_acceptor;
+
+    void OnAccept(boost::system::error_code ec, boost::asio::ip::tcp::socket socket);
+
+public:
+    Server(std::string&& address, uint16_t port, int concurrency = 1);
+    //Server(uint16_t port, int concurrency = 1);
+
+    //~Server();
+
+    //void Accept();
+    //void Start();
+    //void Stop();
+    //void Abort();//TODO: Remove?
+};
+
+#pragma region Server
+
+Server::Server(std::string&& address, uint16_t port, int concurrency)
+    : m_ioThreadCount { 1 }
+{
+
+}
+
+#pragma endregion Server
 
 // See example at https://www.boost.org/doc/libs/1_77_0/doc/html/boost_asio/reference/ip__tcp/acceptor.html
 int main()
@@ -13,4 +46,6 @@ int main()
     acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
     acceptor.bind(endpoint);
     acceptor.listen();
+
+    auto server = std::make_shared<Server>("0.0.0.0", 1770, 1);
 }
