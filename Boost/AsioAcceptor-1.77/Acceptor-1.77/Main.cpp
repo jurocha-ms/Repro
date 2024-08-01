1687053902957f42784a76d0124b53c69be9b43f
Julio C. Rocha <julio.rocha@microsoft.com>
2024-07-31 20:23:26 -0700

    Add AsioAcceptor-1.77

---
+++ Boost/AsioAcceptor-1.77/Acceptor-1.77/Main.cpp
@@ -0,0 +1,16 @@
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+// See example at https://www.boost.org/doc/libs/1_77_0/doc/html/boost_asio/reference/ip__tcp/acceptor.html
+int main()
+{
+    boost::asio::io_context my_context{};
+    int port = 1770;
+
+    boost::asio::ip::tcp::acceptor acceptor(my_context);
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
+    acceptor.open(endpoint.protocol());
+    acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    acceptor.bind(endpoint);
+    acceptor.listen();
+}
